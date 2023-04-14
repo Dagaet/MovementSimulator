8ff90f90571eb03ff2521eddc59335c84ad2adfb
SaulArteaga <alojera15@gmail.com>
2023-04-14 08:46:55 +0100

    feat: Map generator half made

--- src/game/character/Character.java
+++ src/game/character/Character.java
@@ -1,5 +1,49 @@
 package game.character;
 
 public class Character {
-    
+
+    protected String name;
+    protected int coordinatey = 0;
+    protected int coordinatex = 0;
+    protected int vel = 1;
+
+    public Character(String name, int coordinatey, int coordinatex, int vel) {
+        this.name = name;
+        this.coordinatey = coordinatey;
+        this.coordinatex = coordinatex;
+        this.vel = vel;
+    }
+
+    public String getName(String name) {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCoordinatey() {
+        return coordinatey;
+    }
+
+    public void setCoordinatey(int coordinatey) {
+        this.coordinatey = coordinatey;
+    }
+
+    public int getCoordinatex() {
+        return coordinatex;
+    }
+
+    public void setCoordinatex(int coordinatex) {
+        this.coordinatex = coordinatex;
+    }
+
+    public int getVel() {
+        return vel;
+    }
+
+    public void setVel(int vel) {
+        this.vel = vel;
+    }
+
 }
--- src/game/utils/Map.java
+++ src/game/utils/Map.java
@@ -1,5 +1,30 @@
 package game.utils;
 
 public class Map {
-    
+    // Implementa una clase Mapa que cumpla con las siguientes condiciones:
+    // a) Debe contener, como mínimo, los siguientes atributos:
+    // - Tamaño del mapa
+    // - Mapa generado
+    // b) El constructor debe recibir el tamaño del mapa. Se debe inicializar aquí
+    // el
+    // atributo del mapa, mediante un método estático que se encuentra en la clase
+    // del
+    // siguiente apartado.
+
+    int mapSize;
+    String[][] generatedMap = MapGenerator.mapGenerated(3);
+
+    public Map(int mapSize) {
+        this.mapSize = mapSize;
+    }
+
+    // public void mostrarMatriz(){
+    // for (int i = 0; i < matriz4x4.length; i++) {
+    // for (int j = 0; j < matriz4x4[0].length; j++) {
+    // System.out.print(matriz4x4[i][j] + " ");
+    // }
+    // System.out.println(" ");
+    // }
+    // }
+
 }
