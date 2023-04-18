5116de1abfcb5be7eb5749e78fddd6d93acd160c
SaulArteaga <alojera15@gmail.com>
2023-04-18 08:39:30 +0100

    feat: added gitignore file

--- src/game/control/GameMenu.java
+++ src/game/control/GameMenu.java
@@ -6,7 +6,7 @@ import game.player.Player;
 import game.utils.Map;
 
 public class GameMenu {
-    final private Scanner keyboard = new Scanner(System.in); 
+    final private Scanner keyboard = new Scanner(System.in);
     private Player player;
     private Map map;
     private String[][] endMap;
@@ -36,19 +36,20 @@ public class GameMenu {
     }
 
     public void playerMovement() {
-        System.out.println("Where do you want to move? \n Press w,s,a,d to move to an specific direction and then press enter");
+        System.out.println(
+                "Where do you want to move? \n Press w,s,a,d to move to an specific direction and then press enter");
         String answer = keyboard.nextLine();
 
-        switch (answer){
+        switch (answer) {
             case "w":
-                    player.setCoordinatey(player.getCoordinatey() - player.getVel());
+                player.setCoordinatey(player.getCoordinatey() - player.getVel());
                 break;
             case "s":
                 player.setCoordinatey(player.getCoordinatey() + player.getVel());
                 break;
             case "a":
                 player.setCoordinatex(player.getCoordinatex() - player.getVel());
-                break; 
+                break;
             case "d":
                 player.setCoordinatex(player.getCoordinatex() + player.getVel());
                 break;
@@ -58,13 +59,13 @@ public class GameMenu {
         }
     }
 
-    public String printMap(){
-        String msg= "";
+    public String printMap() {
+        String msg = "";
         for (int i = 0; i < endMap.length; i++) {
             for (int j = 0; j < endMap.length; j++) {
-                if (player.getCoordinatex() == j && player.getCoordinatey() == i && player.getVel()==2){
+                if (player.getCoordinatex() == j && player.getCoordinatey() == i && player.getVel() == 2) {
                     msg += "8 ";
-                } else if (player.getCoordinatex() == j && player.getCoordinatey() == i && player.getVel()==1) {
+                } else if (player.getCoordinatex() == j && player.getCoordinatey() == i && player.getVel() == 1) {
                     msg += "0 ";
                 } else {
                     msg += endMap[i][j] + " ";
@@ -75,7 +76,7 @@ public class GameMenu {
         return msg;
     }
 
-    public void legend(){
+    public void legend() {
         System.out.println("# -> Map border (not accessible)");
         System.out.println("X -> Map's floor (accessible)");
         System.out.println("O -> Players position (by foot)");
